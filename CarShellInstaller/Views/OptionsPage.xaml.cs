42e6585891f25807c31ce88a0e351d090dd52207
D1MONSHT <63292828+D1MONSHT@users.noreply.github.com>
2026-07-22 12:08:00 +0200

    Update OptionsPage to save options to MainWindow

--- CarShellInstaller/Views/OptionsPage.xaml.cs
+++ CarShellInstaller/Views/OptionsPage.xaml.cs
@@ -6,50 +6,27 @@ namespace CarShellInstaller.Views
     public partial class OptionsPage : Page
     {
         private readonly MainWindow _mainWindow;
-        private readonly InstallPage _installPage;
-        
-        public string SelectedProfile { get; private set; } = "Standard";
-        public bool CreateBackup { get; private set; } = true;
-        public bool CreateRestorePoint { get; private set; } = true;
-        public bool DisableServices { get; private set; } = true;
-        public bool OptimizeWindows { get; private set; } = true;
-        public bool ConfigurePower { get; private set; } = true;
-        public bool ApplyRegistryTweaks { get; private set; } = true;
 
         public OptionsPage(MainWindow mainWindow)
         {
             InitializeComponent();
             _mainWindow = mainWindow;
             
-            // Initialize default values from UI
-            CmbProfile.SelectionChanged += (s, e) => 
+            CmbProfile.SelectionChanged += (s, e) =>
             {
                 if (CmbProfile.SelectedItem is ComboBoxItem item)
                 {
-                    SelectedProfile = item.Content.ToString();
+                    _mainWindow.SelectedProfile = item.Content.ToString();
                 }
             };
         }
 
         private void BtnNext_Click(object sender, RoutedEventArgs e)
         {
-            // Get current selections
-            SelectedProfile = (CmbProfile.SelectedItem as ComboBoxItem)?.Content.ToString() ?? "Standard";
-            CreateBackup = ChkCreateBackup.IsChecked ?? false;
-            CreateRestorePoint = ChkRestorePoint.IsChecked ?? false;
-            DisableServices = ChkDisableServices.IsChecked ?? false;
-            OptimizeWindows = ChkOptimizeWindows.IsChecked ?? false;
-            ConfigurePower = ChkPowerManagement.IsChecked ?? false;
-            ApplyRegistryTweaks = ChkRegistryTweaks.IsChecked ?? false;
+            _mainWindow.SelectedProfile = (CmbProfile.SelectedItem as ComboBoxItem)?.Content.ToString() ?? "Standard";
+            _mainWindow.CreateBackup = ChkCreateBackup.IsChecked ?? false;
+            _mainWindow.CreateRestorePoint = ChkRestorePoint.IsChecked ?? false;
             
-            // Create install page with options
-            var installPage = new InstallPage(_mainWindow);
-            installPage.SetOptions(
-                _mainWindow.SelectedMode, 
-                SelectedProfile, 
-                CreateBackup,
-                CreateRestorePoint
-            );
             _mainWindow.NavigateToInstall();
         }
 
