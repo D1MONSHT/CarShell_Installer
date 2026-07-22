fd4071499476bab85ddd822d714fe45de598c328
D1MONSHT <63292828+D1MONSHT@users.noreply.github.com>
2026-07-22 09:48:35 +0200

    Add App and MainWindow files

---
+++ CarShellInstaller/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Windows;
+using System.Windows.Navigation;
+using CarShellInstaller.Views;
+
+namespace CarShellInstaller
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new WelcomePage(this));
+        }
+
+        public void NavigateTo(Page page)
+        {
+            MainFrame.Navigate(page);
+        }
+
+        public void NavigateToModeSelection()
+        {
+            MainFrame.Navigate(new ModePage(this));
+        }
+
+        public void NavigateToOptions()
+        {
+            MainFrame.Navigate(new OptionsPage(this));
+        }
+
+        public void NavigateToInstall()
+        {
+            MainFrame.Navigate(new InstallPage(this));
+        }
+
+        public void NavigateToFinish()
+        {
+            MainFrame.Navigate(new FinishPage(this));
+        }
+    }
+}
